66557183b3233639409e2131ae4e3cb4b18b180f
Drac16 <angeldrc1612@gmail.com>
2025-10-27 10:32:38 -0500

    deploy: coso

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,24 @@
-import TodoItem from "./TodoItem"
-import { useState } from "react"
+import Navbar from "./components/Navbar";
+import Courses from "./pages/Courses";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
+import "./index.css";
 
-export default function App() {
-
-  const [tareas, setTareas] = useState([]);
-
-  const [input, setInput] = useState("");
-
-
-  const   agregarTarea = () => {
-
-    if (input.trim()) {
-      setTareas([...tareas, { id: Date.now(), text: input.trim(), completed: false }]);
-      setInput("");
-    };
-
-  }
-
-
-  const toggleCompleted = (id) => {
-    setTareas(
-      tareas.map((tarea) =>
-        tarea.id === id ? { ...tarea, completed: !tarea.completed } : tarea
-      )
-    );
-  };
-
-
-  const eliminarTarea = (id) => {
-    setTareas(tareas.filter((tarea) => tarea.id !== id));
-
-  }
-
+function App() {
   return (
-    <div className="max-w-md mx-auto mt-10 p-2  rounded shadow">
-      <h1 className="text-3xl font-bold mb-5 text-center">LIST APP</h1>
-      <div className="flex gap-3 mb-5">
-        <input className="flex-1 p-2 border rounded" type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Añadir Tarea" />
-        <button className="bg-blue-500 text-white px-4 p-y-2 rounded" onClick={agregarTarea} >Añadir Tareas</button>
-      </div>
-
-      <div className="space-y-2 ">
-        {tareas.map((tarea) => (<TodoItem key={tarea.id} tarea={tarea} toggleCompleted={toggleCompleted} eliminarTarea={eliminarTarea} />))}
-      </div>
-
-    </div>
-  )
+    <>
+      <Navbar />
+      <main>
+        <section className="hero">
+          <h1>Centro de Gestión de Mercados, Logística y Tecnologías</h1>
+          <p>Bienvenido a nuestra plataforma de formación</p>
+        </section>
+        <Courses />
+        <ContactForm />
+      </main>
+      <Footer />
+    </>
+  );
 }
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./../styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">SENA</div>
+      <ul className="nav-links">
+        <li>Inicio</li>
+        <li>Propuestas</li>
+        <li>Acerca</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+}
